82fc75506eac834f78d6cf234936fe6cc07ea769
alexey <aleksei.yuiuzhanin@yandex.ru>
2024-09-07 21:18:17 +0700

    commit

--- storage/files/file.go
+++ storage/files/file.go
@@ -3,13 +3,20 @@ package files
 import (
 	err2 "awesomeProject4/lib/err"
 	"awesomeProject4/storage"
+	"encoding/gob"
+	"errors"
+	"fmt"
+	"math/rand"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 const (
 	defaultPerm = 0774
 )
+
+var ErrNoSavedPages = errors.New("No saved pages")
 
 type Storage struct {
 	basePath string
@@ -20,7 +27,7 @@ func New(basePath string) *Storage {
 }
 
 func (storage *Storage) Save(p *storage.Page) (err error) {
-	defer func() { err = err2.WrapIfErr(err, "cant save") }()
+	defer func() { err = err2.WrapIfErr(err, "cant save page") }()
 
 	filePath := filepath.Join(storage.basePath, p.UserName)
 
@@ -28,4 +35,92 @@ func (storage *Storage) Save(p *storage.Page) (err error) {
 		return err
 	}
 
+	fName, err := fileName(p)
+	if err != nil {
+		return err
+	}
+	filePath = filepath.Join(filePath, fName)
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	if err := gob.NewEncoder(file).Encode(p); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Storage) PickRandom(userName string) (page *storage.Page, err error) {
+	defer func() { err = err2.WrapIfErr(err, "cant pick random page") }()
+
+	fPath := filepath.Join(s.basePath, userName)
+
+	files, err := os.ReadDir(fPath)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(files) == 0 {
+		return nil, ErrNoSavedPages
+	}
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	n := rand.Intn(len(files))
+
+	file := files[n]
+
+	return s.decodePage(filepath.Join(fPath, file.Name()))
+}
+
+func (s *Storage) Remove(page *storage.Page) (err error) {
+	fileName, err := fileName(page)
+	if err != nil {
+		return err2.Wrap(err, "cant remove page")
+	}
+	path := filepath.Join(s.basePath, page.UserName, fileName)
+	if err := os.Remove(path); err != nil {
+		return err2.Wrap(err, fmt.Sprintf("can't remove file %s", path))
+	}
+	return nil
+}
+
+func (s *Storage) IsExists(page *storage.Page) (bool, error) {
+	filename, err := fileName(page)
+	if err != nil {
+		return false, err2.Wrap(err, "cant remove page")
+	}
+	path := filepath.Join(s.basePath, page.UserName, filename)
+
+	switch _, err := os.Stat(path); {
+	case errors.Is(err, os.ErrNotExist):
+		return false, nil
+	case err != nil:
+		return false, err2.Wrap(err, "can't check if file exists")
+	}
+
+	return true, nil
+}
+
+func (s *Storage) decodePage(filePath string) (*storage.Page, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err2.Wrap(err, "Can't open file")
+	}
+	defer f.Close()
+
+	var p storage.Page
+
+	if err := gob.NewDecoder(f).Decode(&p); err != nil {
+		return nil, err2.Wrap(err, "Can't decode file")
+	}
+
+	return &p, nil
+}
+
+func fileName(p *storage.Page) (string, error) {
+	return p.Hash()
 }
--- storage/storage.go
+++ storage/storage.go
@@ -1,5 +1,12 @@
 package storage
 
+import (
+	err2 "awesomeProject4/lib/err"
+	"crypto/sha1"
+	"fmt"
+	"io"
+)
+
 type Storage interface {
 	Save(p *Page) error
 	PickRandom(userName string) (*Page, error)
@@ -10,4 +17,18 @@ type Storage interface {
 type Page struct {
 	URL      string
 	UserName string
+}
+
+func (p *Page) Hash() (string, error) {
+	h := sha1.New()
+
+	if _, err := io.WriteString(h, p.URL); err != nil {
+		return "", err2.Wrap(err, "cant calculate hash")
+	}
+
+	if _, err := io.WriteString(h, p.UserName); err != nil {
+		return "", err2.Wrap(err, "cant calculate hash")
+	}
+
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
 }
