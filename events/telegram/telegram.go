d6eb9c66a66695265b8ef80f33ddbb8136e8095d
alexey <aleksei.yuiuzhanin@yandex.ru>
2024-09-08 00:30:33 +0700

    commit

--- events/telegram/telegram.go
+++ events/telegram/telegram.go
@@ -1,10 +1,96 @@
 package telegram
 
-import "awesomeProject4/cleints/telegram"
+import (
+	"awesomeProject4/cleints/telegram"
+	"awesomeProject4/events"
+	err2 "awesomeProject4/lib/err"
+	"awesomeProject4/storage"
+	"errors"
+)
+
+var ErrUnknown = errors.New("unknown event")
 
 type Processor struct {
-	tg     *telegram.Client
-	offset int
+	tg      *telegram.Client
+	offset  int
+	storage storage.Storage
 }
 
-func New(tg *telegram.Client, offset int) *Processor {}
+type Meta struct {
+	ChatID   int
+	Username string
+}
+
+func New(tg *telegram.Client, storage storage.Storage) *Processor {
+	return &Processor{
+		tg:      tg,
+		offset:  0,
+		storage: storage,
+	}
+}
+
+func (p *Processor) Fetch(limit int) ([]events.Event, error) {
+	update, err := p.tg.Updates(p.offset, limit)
+	if err != nil {
+		return nil, err2.Wrap(err, "cant get events")
+	}
+
+	if len(update) == 0 {
+		return nil, nil
+	}
+
+	res := make([]events.Event, 0, len(update))
+
+	for _, update := range update {
+		res = append(res, event(update))
+	}
+
+	p.offset = update[len(update)-1].ID + 1
+
+	return res, nil
+}
+
+func (p *Processor) Process(event events.Event) error {
+	switch event.Type {
+	case events.Message:
+		p.processMessage(event)
+	default:
+		return ErrUnknown
+	}
+}
+
+func (p *Processor) processMessage(event events.Event) {
+
+}
+
+func event(update telegram.Update) events.Event {
+	updateType := fetchType(update)
+	res := events.Event{
+		Type: updateType,
+		Text: fetchText(update),
+	}
+
+	if updateType == events.Message {
+		res.Meta = Meta{
+			ChatID:   update.Message.Chat.ID,
+			Username: update.Message.From.Username,
+		}
+	}
+
+	return res
+}
+
+func fetchText(update telegram.Update) string {
+	if update.Message == nil {
+		return ""
+	}
+	return update.Message.Text
+}
+
+func fetchType(update telegram.Update) events.Type {
+	if update.Message == nil {
+		return events.Unknown
+	}
+
+	return events.Message
+}
